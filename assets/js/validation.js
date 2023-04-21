1cbd7a885f37a4adad98da4f21647f5f06051c4c
Hamza <hamza@sefion.com>
2023-04-21 00:02:50 -0400

    update validation

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -1,56 +1,74 @@
-const form = document.querySelector('#form-contact');
-const usernameInput = document.querySelector('#username');
-const phoneInput = document.querySelector('#phone');
-const emailInput = document.querySelector('#email');
-const messageInput = document.querySelector('#message');
-const usernameError = document.querySelector('#username-error');
-const phoneError = document.querySelector('#phone-error');
-const emailError = document.querySelector('#email-error');
-const messageError = document.querySelector('#message-error');
+const form = document.querySelector("#form-contact");
+const usernameInput = document.querySelector("#username");
+const phoneInput = document.querySelector("#phone");
+const emailInput = document.querySelector("#email");
+const messageInput = document.querySelector("#message");
+const usernameError = document.querySelector("#username-error");
+const phoneError = document.querySelector("#phone-error");
+const emailError = document.querySelector("#email-error");
+const messageError = document.querySelector("#message-error");
 
-form.addEventListener('submit', (event) => {
+function clearErrors() {
+  usernameError.textContent = "";
+  phoneError.textContent = "";
+  emailError.textContent = "";
+  messageError.textContent = "";
+}
+
+form.addEventListener("submit", (event) => {
   let isValid = true;
   event.preventDefault();
-
-  if (usernameInput.value === '') {
-    usernameError.textContent = 'Please enter your name';
+  clearErrors();
+  if (usernameInput.value === "") {
+    usernameError.textContent = "Please enter your name";
     isValid = false;
   } else {
-    usernameError.textContent = '';
+    usernameError.textContent = "";
   }
 
-  if (phoneInput.value === '') {
-    phoneError.textContent = 'Please enter your phone number';
+  if (phoneInput.value === "") {
+    phoneError.textContent = "Please enter your phone number";
     isValid = false;
   } else {
-    phoneError.textContent = '';
+    phoneError.textContent = "";
   }
 
-  if (emailInput.value === '') {
-    emailError.textContent = 'Please enter your email address';
+  if (emailInput.value === "") {
+    emailError.textContent = "Please enter your email address";
     isValid = false;
   } else if (!isValidEmail(emailInput.value)) {
-    emailError.textContent = 'Please enter a valid email address';
+    emailError.textContent = "Please enter a valid email address";
     isValid = false;
   } else {
-    emailError.textContent = '';
+    emailError.textContent = "";
   }
 
-  if (messageInput.value === '') {
-    messageError.textContent = 'Please enter your message';
+  if (messageInput.value === "") {
+    messageError.textContent = "Please enter your message";
     isValid = false;
   } else {
-    messageError.textContent = '';
+    messageError.textContent = "";
   }
-
   if (isValid) {
-    Swal.fire({
-      position: 'top-end',
-      icon: 'success',
-      title: 'Thank you for Contacting Us',
-      showConfirmButton: false,
-      timer: 1500
-    })
+    // clear inputs
+    usernameInput.value = "";
+    phoneInput.value = "";
+    emailInput.value = "";
+    messageInput.value = "";
+
+    emailjs.sendForm("test_project1email", "template_e7cugi9", form).then(
+      // If the form submission is successful, hide the booking form and display the booking confirmation div.
+      function () {
+        console.log("success");
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Thank you for Contacting Us",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    );
   }
 });
 
